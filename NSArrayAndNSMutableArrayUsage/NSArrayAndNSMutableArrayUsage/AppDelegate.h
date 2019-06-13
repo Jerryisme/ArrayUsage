2d06375b2b109f92cefcb86b44426d98c99f65f6
zhairui <zhairui@globmate.com>
2019-06-13 17:45:24 +0800

    修复

--- NSArrayAndNSMutableArrayUsage/NSArrayAndNSMutableArrayUsage/AppDelegate.h
+++ NSArrayAndNSMutableArrayUsage/NSArrayAndNSMutableArrayUsage/AppDelegate.h
@@ -2,7 +2,7 @@
 //  AppDelegate.h
 //  NSArrayAndNSMutableArrayUsage
 //
-// github地址:https://github.com/Jerryisme/UIButtonEdgeInsets
+// github地址:https://github.com/Jerryisme/ArrayUsage
 // 简书地址:https://www.jianshu.com/p/d319c13d5ee1
 // 简书个人主页:https://www.jianshu.com/u/281c41cc90bc
 
